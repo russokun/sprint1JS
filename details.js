19360169c503ead58587d5c578a5c6f0161eda1a
russokun <agustin.russo2323@gmail.com>
2024-04-08 19:19:03 -0400

    Se añade el buscador por texto y por genero de pelicula, funcionan por individual y complementado, ademas de añadir la pagina details para cada movie.

---
+++ details.js
@@ -0,0 +1,68 @@
+
+// Obtener los parámetros de búsqueda de la URL
+const url = new URLSearchParams(window.location.search);
+const id = url.get('id');
+
+let movie = (array, key) => array.find( obj=> obj.id == key)
+console.log(movie(movies, id))
+let crearCard = (objeto) => {
+  // Crear el HTML de la card
+  let cardHTML = `
+  <div class="max-w-4xl mx-auto p-8 bg-white shadow-lg rounded-lg mt-15 mb-5 max-h-[calc(100vh-200px)] overflow-auto">
+        <img src="${objeto.image}" alt="${objeto.title}" class="w-64 h-auto float-left mr-8 mb-4">
+        <h2 class="text-2xl font-semibold mb-4">${objeto.title}</h2>
+        <span class="bg-[#D2CCFF] font-black text-gray-700 px-2 py-1 rounded mr-2 self-center">${objeto.tagline}</span>
+        <h3 class="text-lg font-semibold mt-4 mb-2">Genres:</h3>
+        <p class="text-gray-700 mb-4">${objeto.genres}</p>
+        <div class="float-left flex flex-col">
+          <div class="mb-4">
+          </div>
+          <p class="text-gray-700 mb-4">${objeto.overview}</p>
+        </div>
+        <div class="clear-both">
+          <h3 class="text-lg font-semibold mb-4">Aditional Details:</h3>
+          <table class="w-full border-collapse border border-solid border-gray-500 mb-5 ">
+            <tr class="border-collapse border border-solid border-gray-500">
+              <td class="font-semibold border-collapse border border-solid border-gray-500">Original Language:</td>
+              <td>${objeto.original_language}</td>
+            </tr>
+            <tr class="border-collapse border border-solid border-gray-500">
+              <td class="font-semibold border-collapse border border-solid border-gray-500">Release Date:</td>
+              <td>${objeto.release_date}</td>
+            </tr>
+            <tr class="border-collapse border border-solid border-gray-500">
+              <td class="font-semibold border-collapse border border-solid border-gray-500">Runtime:</td>
+              <td>${objeto.runtime} mins</td>
+            </tr>
+            <tr>
+              <td class="font-semibold border-collapse border border-solid border-gray-500">Status:</td>
+              <td>${objeto.status}</td>
+            </tr>
+          </table>
+        </div>
+        <table class="w-full border-collapse border border-solid border-gray-500">
+          <tr class="border-collapse border border-solid border-gray-500">
+            <td class="font-semibold border-collapse border border-solid border-gray-500">Vote Average:</td>
+            <td>${objeto.vote_average}</td>
+          </tr>
+          <tr class="border-collapse border border-solid border-gray-500">
+            <td class="font-semibold border-collapse border border-solid border-gray-500">Budget:</td>
+            <td>USD ${objeto.budget}</td>
+          </tr>
+          <tr class="border-collapse border border-solid border-gray-500">
+            <td class="font-semibold border-collapse border border-solid border-gray-500">Revenue:</td>
+            <td>USD ${objeto.revenue}</td>
+          </tr>
+        </table>
+      </div>`;
+  return cardHTML; // Devolver el HTML de la card
+};
+
+let contenedor = document.getElementById("main")
+contenedor.innerHTML = crearCard(movie(movies, id))
+//<img class="w-full h-56 object-cover object-center" src="${objeto.image}" alt="${objeto.title}">
+//          <div class="px-6 py-4">
+//              <h3 class="text-xl font-semibold text-gray-800 mb-2">${objeto.title}</h3>
+//              <h4 class="text-xl font-semibold text-gray-800 mb-2">${objeto.tagline}</h4>
+//              <p class="text-gray-600 text-base">${objeto.overview}</p>
+//          </div>
--- index.js
+++ index.js
@@ -1,25 +1,90 @@
-function crearCard(title, image, tagline, overview){
+let crearCard = (title, image, tagline, overview, id) => {
+  // Limitar la descripción (overview) a 50 caracteres
+  const overviewLimitado = overview.length > 50 ? overview.slice(0, 50) + '...' : overview;
+  // Crear el HTML de la card
   let cardHTML = `
-      <div class="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden mr-4 mb-4">
-        <img class="w-full h-56 object-cover object-center" src="${image}" alt="${title}">
-        <div class="px-6 py-4">
-          <h3 class="text-xl font-semibold text-gray-800 mb-2">${title}<h3>
-          <h4 class="text-xl font-semibold text-gray-800 mb-2">${tagline}<h4>
-          <p class="text-gray-600 text-base">${overview}</p>
-        </div>
-      </div>
-    `
-    return cardHTML
-}
+      <div class="relative max-w-xs bg-[#ADADAD] shadow-lg rounded-lg overflow-hidden mr-4 mb-4">
+          <img class="w-full h-56 object-cover object-center" src="${image}" alt="${title}">
+          <div class="px-6 py-4">
+              <h3 class="text-xl font-semibold text-[#FFFFFF] mb-2">${title}</h3>
+              <h4 class="text-xl font-semibold text-[#FFFFFF] mb-2">${tagline}</h4>
+              <p class="text-[#efefef] text-base">${overviewLimitado}</p>
+              <a href="details.html?id=${id}" class="w-[5.5rem] absolute bottom-0 right-0 bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Details</a>
+          </div>
+      </div>`;
+  return cardHTML; // Devolver el HTML de la card
+};
 function crearCardsMovies(moviesArray) {
-  let cardsHTML = '' // Variable para almacenar todas las cards
+  let cardsHTML = ''// Variable para almacenar todas las cards
   // Iterar sobre el array de movies y generar una card para cada una
-  moviesArray.forEach(function(movie) {
-      // Llamar a la función crearCard() con los datos de cada fruta y concatenar el HTML resultante
-      cardsHTML += crearCard(movie.title, movie.image, movie.tagline, movie.overview);
-  });
+  moviesArray.forEach(function(movie){
+    // Llamar a la función crearCard() con los datos de cada movie y concatenar el HTML resultante
+    cardsHTML += crearCard(movie.title, movie.image, movie.tagline, movie.overview, movie.id)
+  })
   // Devolver el HTML de todas las cards generadas
-  return cardsHTML;
+  return cardsHTML
 }
 const contenedor = document.getElementById("contenedor")
 contenedor.innerHTML = crearCardsMovies(movies)
+//sprint2
+
+// Utilizamos flat() y map() para obtener una lista plana de todos los géneros de todas las películas
+const todosLosGeneros = movies.map(pelicula => pelicula.genres).flat()
+
+// Utilizamos Set para eliminar duplicados y luego el operador de propagación para descomponer los géneros únicos
+const generosUnicos = [...new Set(todosLosGeneros)]
+
+const selectGenero = document.getElementById("selectGeneros");
+
+// Agregar una opción "All" por defecto y seleccionarla en el select de géneros
+const optionAll = document.createElement('option');
+optionAll.text = 'All';
+optionAll.value = 'all';
+optionAll.selected = true;
+document.getElementById('selectGeneros').add(optionAll);
+
+// Itera a través de los géneros y crea las opciones
+generosUnicos.forEach((genero) => {
+  const option = document.createElement("option");
+  option.value = genero; // El valor de la opción es el género
+  option.textContent = genero; // El texto visible de la opción es el género
+  selectGenero.appendChild(option); // Agrega la opción al select
+});
+
+// Variable para almacenar el género seleccionado actualmente
+let generoActual = 'all';
+
+// Trigger del evento change para mostrar todas las películas por defecto al seleccionar "All"
+document.getElementById('selectGeneros').dispatchEvent(new Event('change'));
+
+// Trigger del evento input para mostrar todas las películas por defecto al cargar la página
+document.getElementById('search').dispatchEvent(new Event('input'));
+
+// Función para renderizar las películas según el género actual y el texto de búsqueda
+function renderizarPeliculas() {
+  const textoBusqueda = document.getElementById('search').value.trim().toLowerCase();
+  
+  // Filtrar las películas según el género actual y el texto de búsqueda
+  let peliculasFiltradas = [];
+  if (generoActual === 'all') {
+    peliculasFiltradas = movies;
+  }
+  else {
+    peliculasFiltradas = movies.filter(movie => movie.genres.includes(generoActual));
+  }
+  peliculasFiltradas = peliculasFiltradas.filter(movie => movie.title.toLowerCase().includes(textoBusqueda));
+
+  // Renderizar las películas filtradas
+  const contenedor = document.getElementById("contenedor");
+  contenedor.innerHTML = crearCardsMovies(peliculasFiltradas);
+}
+
+// Event listener para detectar cambios en el campo de búsqueda
+document.getElementById('search').addEventListener('input', renderizarPeliculas);
+
+// Event listener para detectar cambios en el select de géneros
+document.getElementById('selectGeneros').addEventListener('change', function(){
+  generoActual = this.value; // Actualizar el género seleccionado actualmente
+  renderizarPeliculas(); // Renderizar las películas según el género actual y el texto de búsqueda
+});
+
